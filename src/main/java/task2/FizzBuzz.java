9333cf3c2f2efed36261ef54a357e14dbb71191c
Gena Pankratov <gpankratov95@mail.ru>
2021-03-30 12:31:07 +0300

    add second task

--- src/main/java/task2/FizzBuzz.java
+++ src/main/java/task2/FizzBuzz.java
@@ -1,4 +1,85 @@
 package task2;
 
 public class FizzBuzz {
+    private volatile int n;
+    private volatile int tmp = 1;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    public synchronized void fizz() {
+        while (tmp <= n) {
+            if(tmp % 3 == 0 && tmp % 5 != 0) {
+                System.out.print("fizz" +",");
+                tmp++;
+                notifyAll();
+            } else {
+                try {
+                    wait(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+
+    }
+    public synchronized void buzz() {
+        while (tmp <= n) {
+            if(tmp % 3 != 0 && tmp % 5 == 0)  {
+                System.out.print("buzz" +",");
+                tmp++;
+                notifyAll();
+            } else {
+                try {
+                    wait(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+    public synchronized void fizzbuzz() {
+        while (tmp <= n) {
+            if(tmp % 3 == 0 && tmp % 5 == 0) {
+                System.out.print("fizzbuzz" +",");
+                tmp++;
+                notifyAll();
+            } else {
+                try {
+                    wait(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+    public synchronized void number() {
+        while (tmp <= n) {
+            if (tmp % 3 == 0 || tmp % 5 == 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.print(tmp +",");
+                tmp ++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz(15);
+        new Thread(fizzBuzz::fizz).start();
+        new Thread(fizzBuzz::buzz).start();
+        new Thread(fizzBuzz::fizzbuzz).start();
+        new Thread(fizzBuzz::number).start();
+    }
 }
