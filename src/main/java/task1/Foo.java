9a07b6279775d0fe85853966b36c6ee378c10df3
Gena Pankratov <gpankratov95@mail.ru>
2021-04-10 14:58:39 +0300

    add something mad)

--- src/main/java/task1/Foo.java
+++ src/main/java/task1/Foo.java
@@ -1,5 +1,7 @@
 package task1;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Foo {
@@ -7,7 +9,7 @@ public class Foo {
     AtomicBoolean f2 = new AtomicBoolean(false);
     AtomicBoolean f3 = new AtomicBoolean(false);
 
-    public  void first() {
+    public synchronized String first() {
         while(!f1.get()) {
             try {
                 wait();
@@ -19,8 +21,9 @@ public class Foo {
         f1.getAndSet(false);
         f2.getAndSet(true);
         notifyAll();
+        return "first";
     }
-    public  void second() {
+    public synchronized String second() {
         while(!f2.get()) {
             try {
                 wait();
@@ -32,9 +35,10 @@ public class Foo {
         f2.getAndSet(false);
         f3.getAndSet(true);
         notifyAll();
+        return "second";
 
     }
-    public  void third() {
+    public synchronized String third() {
         while(!f3.get()) {
             try {
                 wait();
@@ -47,13 +51,37 @@ public class Foo {
         f3.getAndSet(false);
         f1.getAndSet(true);
         notifyAll();
+        return "third";
     }
 
     public static void main(String[] args) {
         Foo foo = new Foo();
-        new Thread(foo::first).start();
-        new Thread(foo::second).start();
-        new Thread(foo::third).start();
+        CompletableFuture<String> future2
+                = CompletableFuture.supplyAsync(foo::second);
+        CompletableFuture<String> future1
+                = CompletableFuture.supplyAsync(foo::first);
+        CompletableFuture<String> future3
+                = CompletableFuture.supplyAsync(foo::third);
+
+        CompletableFuture<Void> combinedFuture
+                = CompletableFuture.allOf(future1,future2,future3);
+
+        try {
+            combinedFuture.get();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }catch (Exception e ) {
+            System.out.println("Something has been crashed");
+        }
+
+        //assertTrue(future1.isDone());
+        //assertTrue(future2.isDone());
+        //assertTrue(future3.isDone());
+
+
     }
 }
 
